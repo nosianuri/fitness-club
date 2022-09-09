44f0b9f54e35b11a281c5a4839252c7d53d03e70
nosianuri <nurinosia@gmail.com>
2022-09-09 14:56:17 +0600

    set right part of banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -7,7 +7,46 @@ const Banner = () => {
         <div className='banner'>
             <div className='left-h'>
                 <Header />
-                <div className='the-best-ad'></div>
+                {/* the best ad */}
+                <div className='the-best-ad'>
+                    <div></div>
+                    <span>the best fitness club in the town</span>
+                </div>
+                {/* Banner Heading */}
+                <div className='banner-text'>
+                    <div>
+                        <span className='stroke-text'>Shape</span>
+                        <span> Your</span>
+                    </div>
+                    <div>
+                        <span>Ideal body</span>
+                    </div>
+                    <div className=''>
+                        <span>
+                            In here we will help you to shape and build your ideal body and live up your life to fullest
+                        </span>
+                    </div>
+                </div>
+                {/* figures */}
+                <div className='figures'>
+                    <div>
+                        <span>+140</span>
+                        <span>expert coachs</span>
+                    </div>
+                    <div>
+                        <span>+978</span>
+                        <span>members joined</span>
+                    </div>
+                    <div>
+                        <span>+50</span>
+                        <span>fitness programs</span>
+                    </div>
+                </div>
+                {/* Banner btn */}
+                <div className='banner-buttons'>
+                    <button className='btn'>Get Started</button>
+                    <button className='btn'>Learn More</button>
+                </div>
             </div>
             <div className='right-h'>right side</div>
         </div>
