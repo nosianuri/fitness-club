185823d2ab0beb9bf5adeb479118627a1a8e3765
nosianuri <nurinosia@gmail.com>
2022-09-09 16:54:39 +0600

    added reasons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Banner from './components/Banner/Banner';
 import Programs from './components/Programs/Programs';
+import Reasons from './components/Reasons/Reasons';
 
 function App() {
   return (
     <div className="App">
       <Banner />
       <Programs />
+      <Reasons />
     </div>
   );
 }
