d06710d1fc8e5b7f8fd57b4bdfd33c777438446f
nosianuri <nurinosia@gmail.com>
2022-09-09 16:12:09 +0600

    set programs

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import './Programs.css';
+import {programsData} from '../../data/programsData';
+import RightArrow from '../../assets/rightArrow.png'
 
 const Programs = () => {
     return (
         <div className='Programs' id='programs'>
             <div className='programs-header'>
-                <span>Explore our</span>
+                <span className='stroke-text'>Explore our</span>
                 <span>Programs</span>
-                <span>to shape you</span>
+                <span className='stroke-text'>to shape you</span>
+            </div>
+
+            <div className='program-categories'>
+                {programsData.map((program)=>(
+                    <div className='category'>
+                        {program.image}
+                        <span>{program.heading}</span>
+                        <span>{program.details}</span>
+                        <div className='join-now'><span>Join Now</span><img src={RightArrow} alt="" /></div>
+                    </div>
+                ))}
             </div>
         </div>
     );
