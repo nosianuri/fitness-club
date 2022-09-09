5023a82f41232160cdbf41bb30d7c9ccf38bf5be
nosianuri <nurinosia@gmail.com>
2022-09-09 17:27:11 +0600

    added plans

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Banner from './components/Banner/Banner';
+import Plans from './components/Plans/Plans';
 import Programs from './components/Programs/Programs';
 import Reasons from './components/Reasons/Reasons';
 
@@ -9,6 +10,7 @@ function App() {
       <Banner />
       <Programs />
       <Reasons />
+      <Plans />
     </div>
   );
 }
