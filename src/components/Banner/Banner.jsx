04ad4dfff8f4634d0d2fd3e29a1a3fc7082eec50
nosianuri <nurinosia@gmail.com>
2022-09-10 09:35:06 +0600

    make responsive

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,12 +5,14 @@ import banner_image from "../../assets/hero_image.png";
 import banner_image_back from "../../assets/hero_image_back.png";
 import Heart from "../../assets/heart.png";
 import Calories from "../../assets/calories.png";
-
 import {motion} from 'framer-motion';
+
 const Banner = () => {
-    const transition = {type: 'spring', duration: 3}
+    const transition = {type: 'spring', duration: 3};
+    const mobile = window.innerWidth<=768 ? true: false;
+    
     return (
-        <div className='banner'>
+        <div className='banner' id='home'>
 
         <div className='blur banner-blur'></div>
             <div className='left-h'>
@@ -18,7 +20,7 @@ const Banner = () => {
                 {/* the best ad */}
                 <div className='the-best-ad'>
                     <motion.div
-                    initial={{left: '238px'}}
+                    initial={{left: mobile? "165px": '238px' }}
                     whileInView={{left: '8px'}}
                     transition={{...transition, type: 'tween'}}
                     ></motion.div>
@@ -33,7 +35,7 @@ const Banner = () => {
                     <div>
                         <span>Ideal body</span>
                     </div>
-                    <div className=''>
+                    <div>
                         <span>
                             In here we will help you to shape and build your ideal body and live up your life to fullest
                         </span>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,47 @@
-import React from 'react'
-import './Header.css'; 
+import React, { useState } from 'react'
+import './Header.css';
 import Logo from '../../assets/logo.png';
+import Bars from '../../assets/bars.png';
+import { Link } from "react-scroll";
 
 const Header = () => {
+  const mobile = window.innerWidth <= 768 ? true : false;
+  const [menuOpened, setMenuOpened] = useState(false);
   return (
     <div className='header'>
-        <img className='logo' src={Logo} alt="" />
-
+      <img className='logo' src={Logo} alt="" />
+      {(menuOpened === false && mobile === true) ? (
+        <div
+          style={{
+            backgroundColor: 'var(--appColor)',
+            padding: "0.5rem",
+            borderRadius: "5px",
+          }}
+          onClick={() => setMenuOpened(true)}
+        >
+          <img src={Bars} alt="" style={{ width: '1.5rem', height: '1.5rem' }} />
+        </div>
+      ) : (
         <ul className='header-menu'>
-            <li>Home</li>
-            <li>Programs</li>
-            <li>Why us</li>
-            <li>Plans</li>
-            <li>Testimonials</li>
+          <li><Link
+            onClick={() => setMenuOpened(false)}
+            to='testimonials'
+            span={true}
+            smooth={true}
+            >Home</Link></li>
+          <li onClick={() => setMenuOpened(false)}><Link>Programs</Link></li>
+          <li onClick={() => setMenuOpened(false)}><Link>Why us</Link></li>
+          <li onClick={() => setMenuOpened(false)}><Link>Plans</Link></li>
+          <li><Link
+            onClick={() => setMenuOpened(false)}
+            to='testimonials'
+            span={true}
+            smooth={true}
+          >Testimonials</Link></li>
         </ul>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
