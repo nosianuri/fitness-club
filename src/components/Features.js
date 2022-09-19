d464b162081761de0d05e97eab3804c16674549f
nosianuri <nurinosia@gmail.com>
2022-09-20 01:52:37 +0600

    video added

--- src/components/Features.js
+++ src/components/Features.js
@@ -4,28 +4,36 @@ import '../video.scss';
 import { BsPlayCircleFill } from 'react-icons/bs';
 
 const Features = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <section>
+    <section className='bg-section min-h-[428px] pt-8 pb-[38px] lg:pb-[150px] mt-[120px] lg:mt-[130px]'>
       <div className='container mx-auto'>
-        <div className='bg-video h-[310px] bg-cover'>
-          <div>
+        <div className='bg-video h-[310px] bg-cover bg-center bg-no-repeat lg:h-[622px] w-full flex justify-center items-center rounded-md -mt-[150px] mb-[28px] lg:mb-20'>
+          <div onClick={() => setIsOpen(!isOpen)} className='text-6xl lg:text-[120px] cursor-pointer hover:scale-110 transition text-orange'>
             <BsPlayCircleFill />
           </div>
         </div>
-        <div>
-          <div>
-            <h2>
+        <div className='flex flex-col lg:flex-row lg:gap-x-8'>
+          <div className='flex-1'>
+            <div className='w-9 h-[2px] bg-orange mb-2 lg:w-[70px] rounded-full'></div>
+            <h2 className='h2 mb-3 lg:mb-0'>
               The Better Way to <br /> Start Yoga
             </h2>
-            <div>
-              <p>
-                Practice anywhere, anytime. Explore a new way to exercise and learn more about yourself. We are providing the best.
-              </p>
-              <button>Get started</button>
-            </div>
+          </div>
+          <div className='flex-1 '>
+            <p className='max-w-[360px] mb-[18px] lg:mb-[38px]'>
+              Practice anywhere, anytime. Explore a new way to exercise and learn more about yourself. We are providing the best.
+            </p>
+            <button className='btn btn-sm btn-orange'>Get started</button>
           </div>
         </div>
       </div>
+      <ModalVideo
+        channel='youtube'
+        autoplay
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        videoId='klmBssEYkdU' />
     </section>
   );
 };
